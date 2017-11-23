c2d9301ffcce3f909bfa45aeab57999b6fb0545e
Jitesh Gupta <jiteshgupta1995@gmail.com>
2017-11-23 16:09:53 +0530

    code refinement

--- src/components/homeComponent/dashboardComponent.js
+++ src/components/homeComponent/dashboardComponent.js
@@ -3,29 +3,29 @@ import RaisedButton from "material-ui/RaisedButton";
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import PropTypes from "prop-types";
 
-const dashboardComponent = (props) =>{
+const dashboardComponent = ({user, signout, onChangeHandler, list}) =>{
     return(
         <MuiThemeProvider>
             <div className="row">
                 <div className="row">
                     <div className="col-xs-offset-4 col-xs-4">
-                        <h1>Welcome! {props.user}</h1>
+                        <h1>Welcome! {user}</h1>
                     </div>
                     <div className="col-xs-4">
                         <RaisedButton className="float-right" label="Signout" primary={true}
-                            onClick={() => props.signout()}/>
+                            onClick={signout}/>
                     </div>
                 </div>
                 <div className="row">
                     <div className="form-group">
                         <label>Enter text</label>
                         <input type="text" className="form-control" id="userInput"
-                            onChange={props.onChangeHandler}
+                            onChange={onChangeHandler}
                         />
                     </div>
                 </div>
                 <div className="row">
-                    {props.list}
+                    {list}
                 </div>
             </div>
         </MuiThemeProvider>
