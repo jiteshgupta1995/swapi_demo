36ed022e9c69d4ba605ed21e19a93705e5a44673
Jitesh Gupta <jiteshgupta1995@gmail.com>
2017-11-30 17:46:02 +0530

    test case name changed

--- __tests__/DashboardComponent.test.js
+++ __tests__/DashboardComponent.test.js
@@ -1,22 +1,19 @@
 import React from 'react';
 import { configure, shallow } from 'enzyme';
-import configureStore from 'redux-mock-store';
 import Adapter from 'enzyme-adapter-react-16';
 import DashboardComponent from '../src/components/homeComponent/dashboardComponent';
 
-const mockStore = configureStore();
 configure({ adapter: new Adapter() });
 
-describe('HomeComponent', () => {
+describe('DashboardComponent', () => {
 
-    test('found result from redux store and api is working', () => {
+    it('is rendering search result', () => {
         const items = [
             {name: "Pluto", population: "unknown"},
             {name: "Earth", population: "1000"},
         ];
         const component = shallow(
             <DashboardComponent
-                store={mockStore()}
                 searchItem={items}
             />
         ).instance();
